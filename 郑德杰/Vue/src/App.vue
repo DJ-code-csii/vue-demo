138a8eae4e917583847aa670de701f59da015655
DJ-code-csii <741456520@qq.com>
2020-08-11 11:50:29 +0800

    vue

--- 郑德杰/Vue/src/App.vue
+++ 郑德杰/Vue/src/App.vue
@@ -1,37 +1,33 @@
 <template>
-    <div>
-        <!-- <h2>演示项目</h2>
+  <div>
+    <!-- <h2>演示项目</h2>
         <ul>
             <li class="imp">vue如何搭建</li>
             <li>webpack</li>
             <li>loader</li>
-        </ul> -->
-    <router-view>
-
-
-
-    </router-view>
-    </div>
+    </ul>-->
+    <router-view></router-view>
+  </div>
 </template>>
 <style lang="scss" scoped>
-    // ul>li{
-    //     background-color: blue;
-    //     &.imp{
-    //         color: red;
-    //     }
-    // }
+// ul>li{
+//     background-color: blue;
+//     &.imp{
+//         color: red;
+//     }
+// }
 </style>
 <style scoped>
-    /* 改变当前的div */
-    /* div{
+/* 改变当前的div */
+/* div{
         color: red;
         background-image: url(./assets/zdj.jpg);
         background-size: auto 100%;
     } */
 </style>
 <style>
-    /* 改变index.html里的div */
-     /* div{
+/* 改变index.html里的div */
+/* div{
         color: red;
     } */
 </style>
