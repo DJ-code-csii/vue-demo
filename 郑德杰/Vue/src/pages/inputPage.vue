3e98447a22f0a9b269826028f0bf0da531ab10f7
DJ-code-csii <741456520@qq.com>
2020-08-11 11:42:47 +0800

    vue

--- 郑德杰/Vue/src/pages/inputPage.vue
+++ 郑德杰/Vue/src/pages/inputPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         录入页
-        <span @click="$router.push({path:'/output'})">跳转到结果页</span>
+        <button @click="$router.push({path:'/output'})">跳转到结果页</button>
     </div>
 </template>
--- 郑德杰/Vue/src/pages/outputPage.vue
+++ 郑德杰/Vue/src/pages/outputPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         结果页
+        <button @click="$router.push({path:'/input'})">跳转到录入页</button>
     </div>
 </template>
